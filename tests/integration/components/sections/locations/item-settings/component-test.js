9d80a38c746cb8a41e74db0866833a907cbf1087
Vinh Luu <vinhlt3@gmail.com>
2016-08-08 16:27:24 +0700

    LS-3 fix locations/item-settings test

--- tests/integration/components/sections/locations/item-settings/component-test.js
+++ tests/integration/components/sections/locations/item-settings/component-test.js
@@ -7,7 +7,34 @@ moduleForComponent('sections/locations/item-settings', 'Integration | Component 
 
 test('it renders', function(assert) {
   assert.expect(0);
-  
-  this.render(hbs`{{sections/locations/item-settings}}`);
 
+  this.set('itemSettings', {});
+  this.set('updateItemDesire', () => assert.ok(true));
+  this.set('updateItemCreditRate', () => assert.ok(true));
+  this.set('onSaveMassCreditRate', () => assert.ok(true));
+
+  this.render(hbs`{{sections/locations/item-settings
+    itemSettings=itemSettings
+    onDesireChange=(route-action 'updateItemDesire')
+    onCreditChange=(route-action 'updateItemCreditRate')
+    onSaveMassCreditRate=(route-action 'onSaveMassCreditRate')}}`);
 });
+
+// test('applies mass credit rate', function(assert) {
+//   assert.expect(1);
+//
+//   this.set('itemSettings', {});
+//   this.set('updateItemDesire', () => {});
+//   this.set('updateItemCreditRate', () => {});
+//   this.set('onSaveMassCreditRate', () => assert.ok(true));
+//
+//   this.render(hbs`{{sections/locations/item-settings
+//     itemSettings=itemSettings
+//     onDesireChange=(route-action 'updateItemDesire')
+//     onCreditChange=(route-action 'updateItemCreditRate')
+//     onSaveMassCreditRate=(route-action 'onSaveMassCreditRate')}}`);
+//
+//     this.$('.massCreditRate input').val(20);
+//     // this.$('.massCreditRate button').click();
+//     // debugger;
+// });

130cdf547fd8fae50eb313f43911966a527d0db7
Vinh Luu <vinhlt3@gmail.com>
2016-08-10 15:02:57 +0700

    LS-33 add showing vendor tests

--- app/routes/vendors/show/location.js
+++ app/routes/vendors/show/location.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-const { run } = Ember;
 const INCLUDES = [
   'address',
   'address.visit-windows',

f9ab3f4aa957711e1825b67a4d0a22a0f8acca61
Aram Zadikian <brancusi@gmail.com>
2016-08-11 15:20:55 -0700

    WIP - Routific integration

--- app/mixins/location-hashable.js
+++ app/mixins/location-hashable.js
@@ -6,6 +6,6 @@ export default Ember.Mixin.create({
   locationHash(lat, lng) {
     Ember.assert('Must declare a lat property when using the location-hashable mixin', lat);
     Ember.assert('Must declare a lng property when using the location-hashable mixin', lng);
-    return `~${lat}_${lng}`;
+    return `~${lat}_${lng}`.replace('.', '');
   }
 });

8322d2743838337edb0fd33a4d6d63873301a723
Vinh Luu <vinhlt3@gmail.com>
2016-08-23 21:24:15 +0700

    modify style of distribution page

---
+++ app/components/ui/dd-card/dd-card-title/component.js
@@ -0,0 +1,16 @@
+import Em from "ember";
+import computed from "ember-computed-decorators";
+
+const {
+  computed: { notEmpty }
+} = Em;
+
+export default Em.Component.extend({
+  classNames: ["row"],
+  hasIndex: notEmpty("index"),
+
+  @computed("index")
+  indexFormatted(index) {
+    return S(index + 1).padLeft(2, "0").s;
+  }
+});

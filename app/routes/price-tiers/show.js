91861c89385f0cfa5a2dee2a429a6fc5b46d2d58
Aram Zadikian <brancusi@gmail.com>
2016-08-26 14:35:12 -0700

    Fix single quotes

--- app/routes/price-tiers/show.js
+++ app/routes/price-tiers/show.js
@@ -1,38 +1,38 @@
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import Ember from 'ember';
-import { updateModelField, saveModelIfDirty } from 'last-strawberry/actions/model-actions';
-import { PRODUCT } from 'last-strawberry/constants/item-types';
+import AuthenticatedRouteMixin from "ember-simple-auth/mixins/authenticated-route-mixin";
+import Ember from "ember";
+import { updateModelField, saveModelIfDirty } from "last-strawberry/actions/model-actions";
+import { PRODUCT } from "last-strawberry/constants/item-types";
 
 const MODEL_INCLUDES = [
-  'item-prices',
-  'item-prices.item',
-  'item-prices.price-tier',
-  'item-prices.item.item-prices'
+  "item-prices",
+  "item-prices.item",
+  "item-prices.price-tier",
+  "item-prices.item.item-prices"
 ];
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   setupController(controller, priceTier) {
-    const fulfilledItems = priceTier.get('itemPrices')
-      .map(ip => ip.get('item').content);
+    const fulfilledItems = priceTier.get("itemPrices")
+      .map(ip => ip.get("item").content);
 
-    const allItems = this.store.peekAll('item')
-      .filter(i => i.get('isProduct'))
+    const allItems = this.store.peekAll("item")
+      .filter(i => i.get("isProduct"))
       .toArray();
 
     const openItems = _.difference(allItems, fulfilledItems);
 
     openItems.forEach(item =>
-      this.store.createRecord('item-price', {priceTier, item}));
+      this.store.createRecord("item-price", {priceTier, item}));
 
     this._super(controller, priceTier);
   },
 
   model(params) {
     return Ember.RSVP.all([
-      this.store.query('item', {'filter[tag]':PRODUCT}),
+      this.store.query("item", {"filter[tag]":PRODUCT}),
     ])
     .then(() => {
-      return this.store.findRecord('price-tier', params.id, { reload: true, include:MODEL_INCLUDES.join(',') });
+      return this.store.findRecord("price-tier", params.id, { reload: true, include:MODEL_INCLUDES.join(",") });
     });
   },
 
@@ -40,9 +40,9 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
     updateModelField,
     saveModelIfDirty,
     destroyPriceTier() {
-      this.modelFor('price-tiers.show')
+      this.modelFor("price-tiers.show")
         .destroyRecord()
-        .then(() => this.transitionTo('price-tiers'));
+        .then(() => this.transitionTo("price-tiers"));
     }
   }
 });

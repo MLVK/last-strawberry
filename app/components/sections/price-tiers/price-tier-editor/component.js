dd90fbd741b033b7ca846393da8f112f295cacc5
Vinh Luu <vinhlt3@gmail.com>
2016-09-14 07:41:04 +0700

    show active products only on price tiers page

--- app/components/sections/price-tiers/price-tier-editor/component.js
+++ app/components/sections/price-tiers/price-tier-editor/component.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 import computed from 'ember-computed-decorators';
 
 export default Ember.Component.extend({
-  @computed('model.itemPrices.@each.{isNew}')
+  @computed('model.itemPrices.@each.{isNew,item}')
   openItemPrices(itemPrices = []) {
-    return itemPrices.filter(ip => ip.get('isNew'));
+    return itemPrices.filter(ip => ip.get('isNew') && ip.get("item.active"));
   },
 
-  @computed('model.itemPrices.@each.{isNew}')
+  @computed('model.itemPrices.@each.{isNew,item}')
   fulfilledItemPrices(itemPrices = []) {
-    return itemPrices.filter(ip => !ip.get('isNew'));
+    return itemPrices.filter(ip => !ip.get('isNew') && ip.get("item.active"));
   }
 });

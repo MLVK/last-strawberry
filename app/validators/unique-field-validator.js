524be4e07361c31edb53cf06a6629f7c5b7c365a
Aram Zadikian <brancusi@gmail.com>
2016-09-24 13:24:15 +0700

    Add defer logic

--- app/validators/unique-field-validator.js
+++ app/validators/unique-field-validator.js
@@ -9,12 +9,13 @@ export default Ember.Object.extend({
 
   init() {
     this.subject = new Rx.Subject();
+
     this.subscription = this.subject
       .filter(value => !!value)
       .filter(value => this.get("whitelist").every(item => item !== value))
       .do(value => this.set("isValid", false))
       .debounce(this.get("debounce"))
-      .map(value => this.sendRequest(value))
+      .flatMap(value => this.sendRequest(value))
       .subscribe(isValid => this.set("isValid", isValid));
   },
 
@@ -29,21 +30,22 @@ export default Ember.Object.extend({
       key: this.get("key"),
       value
     }
+    return Rx.Observable.defer(() => {
+      return new Ember.RSVP.Promise(res => {
+        this.get("session").authorize("authorizer:devise", (headerName, headerValue) => {
+          const headers = {};
+          headers[headerName] = headerValue;
+          const payload = {
+            url:`${config.apiHost}/custom/unique_check`,
+            data,
+            headers,
+            type:"POST"
+          };
 
-    return new Ember.RSVP.Promise(res => {
-      this.get("session").authorize("authorizer:devise", (headerName, headerValue) => {
-        const headers = {};
-        headers[headerName] = headerValue;
-        const payload = {
-          url:`${config.apiHost}/custom/unique_check`,
-          data,
-          headers,
-          type:"POST"
-        };
-
-        Ember.$
-          .ajax(payload)
-          .always(response => res(response.unique === "true" || response.unique === true));
+          Ember.$
+            .ajax(payload)
+            .always(response => res(response.unique === "true" || response.unique === true));
+        });
       });
     });
   },

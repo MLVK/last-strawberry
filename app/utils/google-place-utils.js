b72e269811c7bdc636810a6d9c2c180db1c92c63
“Vinh <vinhlt3@gmail.com>
2016-09-24 10:25:54 +0700

    show error when input an invalid address

--- app/utils/google-place-utils.js
+++ app/utils/google-place-utils.js
@@ -1,15 +1,14 @@
-import Ember from 'ember';
-const { isPresent } = Ember;
+import Ember from "ember";
 const DEFAULT_ADDRESS_MAP = {
-  street_number: 'streetNumber',
-  route: 'streetName',
-  locality: 'city',
-  administrative_area_level_1: 'state',
-  postal_code: 'zip'
+  street_number: "streetNumber",
+  route: "streetName",
+  locality: "city",
+  administrative_area_level_1: "state",
+  postal_code: "zip"
 };
 
 const placeToObject = place => {
-  const components = _.get(place, 'address_components', []);
+  const components = _.get(place, "address_components", []);
   const mapped = Object.keys(DEFAULT_ADDRESS_MAP)
     .map(key => {
       return components
@@ -24,12 +23,13 @@ const placeToObject = place => {
     })
 
   const flattened = R.mergeAll(R.flatten(mapped));
-  const street = `${_.get(flattened, 'streetNumber', '')} ${_.get(flattened, 'streetName', '')}`.trim(),
-        lat = _.get(place, 'geometry.location.lat', () => {})(),
-        lng = _.get(place, 'geometry.location.lng', () => {})();
+  const street = `${_.get(flattened, "streetNumber", "")} ${_.get(flattened, "streetName", "")}`.trim(),
+        lat = _.get(place, "geometry.location.lat", () => {})(),
+        lng = _.get(place, "geometry.location.lng", () => {})();
 
   const final = Object.assign(flattened, { street, lat, lng });
-  return R.filter(isPresent, final);
+
+  return final;
 }
 
 export {
